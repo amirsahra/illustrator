ae4fe4eb5dde22c264c84ba71f8ab97e34ae3b04
amirsahra <amirhosein.sahra@gmail.com>
2023-01-12 11:45:33 +0330

    feat:create service provider

---
+++ src/IllustratorServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Amirsahra\Illustrator;
+
+use Illuminate\Support\ServiceProvider;
+
+class IllustratorServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
